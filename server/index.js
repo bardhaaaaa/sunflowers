f406d27733a179b1c93d83f04e5bbcd074bbe661
ZanfinaD <zd65082@ubt-uni.net>
2025-04-12 18:28:09 +0200

    teachers edhe kids

--- server/index.js
+++ server/index.js
@@ -10,6 +10,12 @@ const db = require('./models');
 const userRouter = require('./routes/Users');
 app.use("/users", userRouter);
 
+const teacherRouter = require('./routes/Teachers');
+app.use("/teachers", teacherRouter);
+
+const kidRouter = require('./routes/Kids');
+app.use("/kids", kidRouter);
+
 db.sequelize.sync().then(() => {
     app.listen(3001, () => {
         console.log("server running on port 3001");
