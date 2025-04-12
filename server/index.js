727a00e6c9c4bd558c698fc3a4a0457e510b8e79
LAPTOP-AGO6BS4U\bardh <bg64910@ubt-uni.net>
2025-04-12 17:58:04 +0200

    fillimi i backendit, lidhja me databaze edhe users

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require('cors');
+
+app.use(express.json());
+app.use(cors());
+
+const db = require('./models');
+
+const userRouter = require('./routes/Users');
+app.use("/users", userRouter);
+
+db.sequelize.sync().then(() => {
+    app.listen(3001, () => {
+        console.log("server running on port 3001");
+    });
+});
