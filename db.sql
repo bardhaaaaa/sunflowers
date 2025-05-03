cf11d11528518a857ff48e386b30310922efa1e7
LAPTOP-AGO6BS4U\bardh <bg64910@ubt-uni.net>
2025-05-03 16:00:40 +0200

    instalimi i client front, pjesa e signinit ne client me tokens edhe databazen e bona export

--- db.sql
+++ db.sql
@@ -16,6 +16,156 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `classes`
+--
+
+DROP TABLE IF EXISTS `classes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `classes` (
+  `classID` int NOT NULL AUTO_INCREMENT,
+  `emri` varchar(255) NOT NULL,
+  `orari` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `classTeacherID` int NOT NULL,
+  PRIMARY KEY (`classID`),
+  KEY `classTeacherID` (`classTeacherID`),
+  CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`classTeacherID`) REFERENCES `teachers` (`teacherID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `classes`
+--
+
+LOCK TABLES `classes` WRITE;
+/*!40000 ALTER TABLE `classes` DISABLE KEYS */;
+INSERT INTO `classes` VALUES (1,'Art Figurativ','9:00 - 10:00','2025-05-03 13:23:16','2025-05-03 13:23:16',1);
+/*!40000 ALTER TABLE `classes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `enrollments`
+--
+
+DROP TABLE IF EXISTS `enrollments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `enrollments` (
+  `enrollmentID` int NOT NULL AUTO_INCREMENT,
+  `data` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `enrollmentKidID` int NOT NULL,
+  `enrollmentClassID` int NOT NULL,
+  PRIMARY KEY (`enrollmentID`),
+  KEY `enrollmentKidID` (`enrollmentKidID`),
+  KEY `enrollmentClassID` (`enrollmentClassID`),
+  CONSTRAINT `enrollments_ibfk_1` FOREIGN KEY (`enrollmentKidID`) REFERENCES `kids` (`kidID`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `enrollments_ibfk_2` FOREIGN KEY (`enrollmentClassID`) REFERENCES `classes` (`classID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `enrollments`
+--
+
+LOCK TABLES `enrollments` WRITE;
+/*!40000 ALTER TABLE `enrollments` DISABLE KEYS */;
+INSERT INTO `enrollments` VALUES (1,'2025-05-02','2025-05-03 13:57:32','2025-05-03 13:57:32',1,1);
+/*!40000 ALTER TABLE `enrollments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kids`
+--
+
+DROP TABLE IF EXISTS `kids`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kids` (
+  `kidID` int NOT NULL AUTO_INCREMENT,
+  `emri` varchar(255) NOT NULL,
+  `mbiemri` varchar(255) NOT NULL,
+  `ditelindja` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  `parentID` int NOT NULL,
+  PRIMARY KEY (`kidID`),
+  KEY `parentID` (`parentID`),
+  CONSTRAINT `kids_ibfk_1` FOREIGN KEY (`parentID`) REFERENCES `users` (`userID`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kids`
+--
+
+LOCK TABLES `kids` WRITE;
+/*!40000 ALTER TABLE `kids` DISABLE KEYS */;
+INSERT INTO `kids` VALUES (1,'fjolla','krasniqi','2013-02-13','2025-05-03 13:57:19','2025-05-03 13:57:19',1);
+/*!40000 ALTER TABLE `kids` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `meals`
+--
+
+DROP TABLE IF EXISTS `meals`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `meals` (
+  `mealID` int NOT NULL AUTO_INCREMENT,
+  `emri` varchar(255) NOT NULL,
+  `pershkrimi` varchar(255) NOT NULL,
+  `orari` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`mealID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `meals`
+--
+
+LOCK TABLES `meals` WRITE;
+/*!40000 ALTER TABLE `meals` DISABLE KEYS */;
+INSERT INTO `meals` VALUES (1,'Veze','Mengjes','09:00','2025-05-03 13:32:54','2025-05-03 13:32:54');
+/*!40000 ALTER TABLE `meals` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `teachers`
+--
+
+DROP TABLE IF EXISTS `teachers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teachers` (
+  `teacherID` int NOT NULL AUTO_INCREMENT,
+  `emri` varchar(255) NOT NULL,
+  `mbiemri` varchar(255) NOT NULL,
+  `nrTel` varchar(255) NOT NULL,
+  `specializimi` varchar(255) NOT NULL,
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL,
+  PRIMARY KEY (`teacherID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `teachers`
+--
+
+LOCK TABLES `teachers` WRITE;
+/*!40000 ALTER TABLE `teachers` DISABLE KEYS */;
+INSERT INTO `teachers` VALUES (1,'hana','shala','045748172','edukim','2025-05-03 13:22:42','2025-05-03 13:22:42');
+/*!40000 ALTER TABLE `teachers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `users`
 --
 
@@ -31,7 +181,7 @@ CREATE TABLE `users` (
   `createdAt` datetime NOT NULL,
   `updatedAt` datetime NOT NULL,
   PRIMARY KEY (`userID`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,7 +190,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'dita','ditanama','12345678','admin','2025-04-12 15:55:22','2025-04-12 15:55:22');
+INSERT INTO `users` VALUES (1,'bardha','b@gmail.com','12345678','User','2025-04-12 15:55:22','2025-05-03 13:22:02'),(3,'zana','z@gmail.com','12345678','Admin','2025-05-03 13:21:44','2025-05-03 13:21:44');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-04-12 18:02:18
+-- Dump completed on 2025-05-03 15:59:35
